2b69476adf87f3145b341993c996b16c6d0652c7
sameernshamsudheen <94037043+sameernshamsudheen@users.noreply.github.com>
2024-09-09 16:56:08 +0530

    quickfix-redis.del i snot working fixed

--- controllers/logout/logout.js
+++ controllers/logout/logout.js
@@ -1,10 +1,15 @@
 const ErrorHandler = require("../../utils/errorHandler");
 const userModal = require("../../models/user/userModel");
 const sendToken = require("../../utils/jwt");
+const { redis } = require("../../redis/redisConnection");
 const Logout = catchAsyncError(async (req, res, next) => {
   try {
     res.cookie("access_token", " ", { maxAge: 1 });
-    res.cookie("refresh_token"," ", { maxAge: 1 });
+    res.cookie("refresh_token", " ", { maxAge: 1 });
+    const userId = req.user?._id || "";
+        console.log(userId ,"userID");
+        
+    redis.del(userId);
     res.status(200).json({
       success: true,
       message: "user logout successFull",
