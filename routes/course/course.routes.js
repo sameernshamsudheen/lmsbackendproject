fb5ef54f125b71494f22b5105d833d8cbb22e865
sameernshamsudheen <94037043+sameernshamsudheen@users.noreply.github.com>
2024-09-12 21:31:07 +0530

    add:singlecoursegetfunction

--- routes/course/course.routes.js
+++ routes/course/course.routes.js
@@ -1,10 +1,14 @@
-const express =require("express")
-const courseRouter=express.Router()
-const {uploadCourse,editCourse}=require("../../controllers/Course/course.controller");
+const express = require("express");
+const courseRouter = express.Router();
+const {
+  uploadCourse,
+  editCourse,
+  getSingleCourse,
+} = require("../../controllers/Course/course.controller");
 const { isAuthenticated, validateUserRole } = require("../../middleware/auth");
 
-courseRouter.post("/create-course",isAuthenticated,uploadCourse)
-courseRouter.put("/edit-course/:id",isAuthenticated,editCourse)
+courseRouter.post("/create-course", isAuthenticated, uploadCourse);
+courseRouter.put("/edit-course/:id", isAuthenticated, editCourse);
+courseRouter.get("/get-single-course/:id", getSingleCourse);
 
-
-module.exports= courseRouter;
+module.exports = courseRouter;
